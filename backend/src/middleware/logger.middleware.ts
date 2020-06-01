732a53da255ed3034b11a217c9080a8d96173519
Josiah Sayers <josiah.sayers15@gmail.com>
2020-05-31 23:31:25 -0400

    changes logger middleware from debug to info call

--- backend/src/middleware/logger.middleware.ts
+++ backend/src/middleware/logger.middleware.ts
@@ -2,7 +2,7 @@ import { Request, Response, NextFunction } from 'express';
 import { logger } from '../util/logger';
 
 export const logRequest = (req: Request, res: Response, next: NextFunction): Response | void => {
-  logger.debug({
+  logger.info({
     method: `${req.method}`,
     path: `${req.originalUrl}`,
     transactionId: req.headers['transaction-id']
