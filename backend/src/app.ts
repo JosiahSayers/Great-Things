a4e4f42c6b82d08685bd4d66f3d4be77b8b66c7d
Josiah Sayers <josiah.sayers15@gmail.com>
2020-06-02 14:03:08 -0400

    Adds cloudinary for file uploads

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import passport from 'passport';
-import { PORT, MONGO_CONNECTION_STRING } from './util/environment';
+import { PORT, MONGO_CONNECTION_STRING, CLOUDINARY_CONIFG } from './util/environment';
 import authController from './controllers/auth.controller';
 import greatThingsController from './controllers/great-things.controller';
 import mongoose from 'mongoose';
@@ -10,6 +10,8 @@ import { logRequest } from './middleware/logger.middleware';
 import { validateHeaders } from './middleware/validation.middleware';
 import { isAuthorized, isCurrentUser } from './middleware/auth.middleware';
 import { logger } from './util/logger';
+import { v2 as cloudinary } from 'cloudinary';
+import fileUpload from 'express-fileupload';
 
 const app = express();
 
@@ -28,10 +30,17 @@ mongoose.connect(MONGO_CONNECTION_STRING, {
     process.exit();
   });
 
+cloudinary.config(CLOUDINARY_CONIFG);
+
 app.set('port', PORT || 3000);
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(passport.initialize());
+app.use(fileUpload({
+  useTempFiles: true,
+  tempFileDir: '/tmp/'
+}));
+app.use(bodyParser.urlencoded());
 
 app.use('/v1/auth', logRequest, validateHeaders, authController);
 app.use('/v1/users/:userid/great-things', logRequest, validateHeaders, isAuthorized, isCurrentUser, greatThingsController);
--- backend/src/util/environment.ts
+++ backend/src/util/environment.ts
@@ -1,4 +1,5 @@
 import dotenv from 'dotenv';
+// import { ConfigOptions } from 'cloudinary';
 
 dotenv.config({ path: '.env' });
 
@@ -6,3 +7,5 @@ export const MONGO_CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING;
 export const PORT = process.env.port;
 export const JWT_SECRET = process.env.JWT_SECRET;
 export const ENVIRONMENT = process.env.ENVIRONMENT;
+
+export const CLOUDINARY_CONIFG = JSON.parse(process.env.CLOUDINARY_CONFIG);
