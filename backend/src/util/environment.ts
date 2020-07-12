af07c1795a5321917d87ce3f3a081a9bfe6f5291
Josiah Sayers <josiah.sayers15@gmail.com>
2020-07-11 23:06:18 -0400

    adds environment files to heroku deploy

--- backend/src/util/environment.ts
+++ backend/src/util/environment.ts
@@ -1,12 +1,15 @@
 import dotenv from 'dotenv';
 // import { ConfigOptions } from 'cloudinary';
 
-dotenv.config({ path: '.env' });
+export const ENVIRONMENT = process.env.ENVIRONMENT;
+
+if (ENVIRONMENT === 'local') {
+  dotenv.config({ path: '.env' });
+}
 
 export const MONGO_CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING;
 export const PORT = process.env.port;
 export const JWT_SECRET = process.env.JWT_SECRET;
-export const ENVIRONMENT = process.env.ENVIRONMENT;
 
 export const GCP_ID = process.env.GOOGLE_CLOUD_PROJECT;
 
