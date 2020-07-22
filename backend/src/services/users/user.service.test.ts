f80126a43e34a0f8b19a5fcefad828095480e138
Josiah Sayers <josiah.sayers15@gmail.com>
2020-07-22 19:07:21 -0400

    adds specs for registration function

--- backend/src/services/users/user.service.test.ts
+++ backend/src/services/users/user.service.test.ts
@@ -1,20 +1,21 @@
 import { logger } from '../../util/logger';
 import { User, UserDocument } from '../../models/User';
 import { UserService } from './user.service';
-import { AuthenticationBody } from '../../types/authentication-body';
 import { UserServiceHelper as helper } from './user-helper.service';
+import { RegisterBody } from '../../types/register-body';
 
 describe('UserService', () => {
   beforeEach(() => {
     logger.info = jest.fn();
     logger.debug = jest.fn();
     logger.error = jest.fn();
-    User.findOne = jest.fn().mockReturnValue(Promise.resolve(mockFoundUser({})));
     helper.buildUserForLog = jest.fn().mockReturnValue('USER_FOR_LOG');
     helper.createJwt = jest.fn().mockReturnValue('JWT');
   });
 
   describe('authenticate', () => {
+    beforeEach(() => User.findOne = jest.fn().mockReturnValue(Promise.resolve(mockUser({}))));
+
     it('throws an error when the username or password is falsy', async () => {
       try {
         await UserService.authenticate(<any>{ body: '' });
@@ -28,7 +29,7 @@ describe('UserService', () => {
       let foundUser: UserDocument;
 
       beforeEach(() => {
-        foundUser = mockFoundUser({
+        foundUser = mockUser({
           email: 'EMAIL',
           password: 'PASSWORD'
         }, true);
@@ -64,7 +65,7 @@ describe('UserService', () => {
       let foundUser: UserDocument;
 
       beforeEach(() => {
-        foundUser = mockFoundUser({
+        foundUser = mockUser({
           email: 'EMAIL',
           password: 'PASSWORD'
         }, false);
@@ -93,7 +94,7 @@ describe('UserService', () => {
             username: 'USERNAME',
             password: 'PASSWORD'
           }));
-        } catch(e) {
+        } catch (e) {
           expect(e.message).toBe('401');
         }
       });
@@ -114,7 +115,7 @@ describe('UserService', () => {
 
       it('logs the error, sets the message to "500" and rethrows when the error is not expected', async () => {
         const testError = new Error('TEST_ERROR');
-        User.findOne = jest.fn().mockImplementation(() => {throw testError;});
+        User.findOne = jest.fn().mockImplementation(() => { throw testError; });
 
         try {
           await UserService.authenticate(mockReq({
@@ -129,13 +130,133 @@ describe('UserService', () => {
       });
     });
   });
+
+  describe('register', () => {
+    let createdUser: UserDocument;
+
+    beforeEach(() => {
+      createdUser = mockUser({
+        email: 'EMAIL',
+        profile: {
+          name: 'NAME',
+          picture: 'PICTURE'
+        }
+      });
+      User.prototype.save = jest.spyOn(User.prototype, 'save').mockResolvedValue(createdUser);
+      helper.isValidEmail = jest.fn().mockResolvedValue(true);
+      helper.isValidPassword = jest.fn().mockReturnValue(true);
+    });
+
+    it('calls helper functions to determine if the email and password is valid', async () => {
+      await UserService.register(mockReq({
+        username: 'USERNAME',
+        password: 'PASSWORD'
+      }));
+
+      expect(helper.isValidEmail).toHaveBeenCalledWith('USERNAME');
+      expect(helper.isValidPassword).toHaveBeenCalledWith('PASSWORD');
+    });
+
+    it('throws an error if the email is invalid', async () => {
+      helper.isValidEmail = jest.fn().mockResolvedValue(false);
+
+      try {
+        await UserService.register(mockReq({}));
+        expect(false).toBe(true);
+      } catch (e) {
+        expect(e.message).toBe('400');
+      }
+    });
+
+    it('logs a debug message if the email or password is invalid', async () => {
+      helper.isValidPassword = jest.fn().mockReturnValue(false);
+      try {
+        await UserService.register(mockReq({ username: 'USERNAME' }));
+      } catch {}
+
+      expect(logger.debug).toHaveBeenCalledWith({
+        msg: 'User tried to register with an invalid email or password',
+        registration: {
+          email: 'USERNAME'
+        },
+        transactionId: 'TRANSACTION_ID'
+      });
+    });
+
+    it('throws an error if the password is invalid', async () => {
+      helper.isValidPassword = jest.fn().mockReturnValue(false);
+
+      try {
+        await UserService.register(mockReq({}));
+      } catch (e) {
+        expect(e.message).toBe('400');
+      }
+    });
+
+    it('saves a new user to the database if the email and password are valid', async () => {
+      await UserService.register(mockReq({
+        username: 'USERNAME',
+        password: 'PASSWORD',
+        name: 'NAME',
+        picture: 'PICTURE'
+      }));
+
+      expect(User.prototype.save).toHaveBeenCalledWith();
+    });
+
+    it('creates a new jwt for the user and returns it', async () => {
+      const returnValue = await UserService.register(mockReq({}));
+      expect(helper.createJwt).toHaveBeenCalledWith(createdUser);
+      expect(returnValue).toBe('JWT');
+    });
+
+    it('logs an info message when a new user is registered', async () => {
+      await UserService.register(mockReq({}));
+      expect(helper.buildUserForLog).toHaveBeenCalledWith({ userDocument: createdUser });
+      expect(logger.info).toHaveBeenCalledWith({
+        msg: 'User successfully registered',
+        registration: 'USER_FOR_LOG',
+        transactionId: 'TRANSACTION_ID'
+      });
+    });
+
+    it('logs an error message if an unhandled exception occurs', async () => {
+      const testError = new Error('TEST_ERROR');
+      helper.isValidPassword = jest.fn().mockImplementation(() => {throw testError;});
+
+      try {
+        await UserService.register(mockReq({
+          username: 'EMAIL',
+          name: 'NAME',
+          picture: 'PICTURE'
+        }));
+      } catch(e) {
+        expect(e.message).toBe('500');
+      }
+
+      expect(logger.error).toHaveBeenCalledWith({
+        msg: 'User tried to register with the following information:',
+        registration: {
+          email: 'EMAIL',
+          profile: {
+            name: 'NAME',
+            picture: 'PICTURE'
+          }
+        },
+        error: testError,
+        transactionId: 'TRANSACTION_ID'
+      });
+    });
+  });
 });
 
-function mockReq(values: Partial<AuthenticationBody>): any {
+function mockReq(values: Partial<RegisterBody>): any {
   return <any>{
     body: {
       username: values.username || '',
-      password: values.password || ''
+      password: values.password || '',
+      name: values.name || '',
+      picture: values.picture || ''
     },
     headers: {
       'transaction-id': 'TRANSACTION_ID'
@@ -143,7 +264,7 @@ function mockReq(values: Partial<AuthenticationBody>): any {
   };
 }
 
-function mockFoundUser(userDetails: Partial<UserDocument>, comparePasswordReturnValue?: boolean): UserDocument {
+function mockUser(userDetails: Partial<UserDocument>, comparePasswordReturnValue?: boolean): UserDocument {
   return <any>{
     comparePassword: () => comparePasswordReturnValue,
     email: userDetails.email || '',
