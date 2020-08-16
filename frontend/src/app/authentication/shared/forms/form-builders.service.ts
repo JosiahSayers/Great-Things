e7c380f6e24b978b6f36cf541ef5247bde2f959f
Josiah Sayers <josiah.sayers15@gmail.com>
2020-08-16 19:42:25 -0400

    adds validators for password field

--- frontend/src/app/authentication/shared/forms/form-builders.service.ts
+++ frontend/src/app/authentication/shared/forms/form-builders.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, ValidatorFn, AbstractControl } from '@angular/forms';
 
 @Injectable()
 export class FormBuildersService {
@@ -15,19 +15,65 @@ export class FormBuildersService {
   loginForm(): FormGroup {
     return new FormGroup({
       email: this.newEmailControl(),
-      password: this.newPasswordControl()
+      password: this.newPasswordControl(false)
     });
   }
 
   private newNameControl(): FormControl {
-    return new FormControl('', [Validators.required]);
+    return new FormControl('', [
+      Validators.required,
+      Validators.maxLength(35)
+    ]);
   }
 
   private newEmailControl(): FormControl {
-    return new FormControl('', [Validators.required]);
+    return new FormControl('', [
+      Validators.required,
+      Validators.minLength(6),
+      Validators.pattern(/^([a-zA-Z0-9_\-\.\+]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/)
+    ]);
   }
 
-  private newPasswordControl(): FormControl {
-    return new FormControl('', [Validators.required]);
+  private newPasswordControl(registrationValidators = true): FormControl {
+    const validators = registrationValidators ? [
+      Validators.required,
+      Validators.minLength(8),
+      this.lowerCaseValidator(),
+      this.upperCaseValidator(),
+      this.numberValidator(),
+      this.specialCharacterValidator()
+    ] : [
+        Validators.required
+      ];
+
+    return new FormControl('', validators);
+  }
+
+  private lowerCaseValidator(): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: any } | null => {
+      const containsLowerCase = /[a-z]/.test(control.value);
+      return !containsLowerCase ? { containsLowerCase: { value: control.value } } : null;
+    };
+  }
+
+  private upperCaseValidator(): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: any } | null => {
+      const containsUpperCase = /[A-Z]/.test(control.value);
+      return !containsUpperCase ? { containsUpperCase: { value: control.value } } : null;
+    };
+  }
+
+  private numberValidator(): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: any } | null => {
+      const containsNumbers = /[0-9]/.test(control.value);
+      return !containsNumbers ? { containsNumbers: { value: control.value } } : null;
+    };
+  }
+
+  private specialCharacterValidator(): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: any } | null => {
+      const containsSpecialCharacter = /[!@#\$%\^&\*]/.test(control.value);
+      return !containsSpecialCharacter ? { containsSpecialCharacters: { value: control.value } } : null;
+    };
   }
 }
