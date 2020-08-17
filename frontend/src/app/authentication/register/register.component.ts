76555fd6989b37eeab83c4f4b1567331813f179f
Josiah Sayers <josiah.sayers15@gmail.com>
2020-08-16 21:23:55 -0400

    adds basic version of error displaying in form

--- frontend/src/app/authentication/register/register.component.ts
+++ frontend/src/app/authentication/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormGroup, AbstractControl } from '@angular/forms';
 import { FormBuildersService } from '../shared/forms/form-builders.service';
 
 @Component({
@@ -23,6 +23,42 @@ export class RegisterComponent implements OnInit {
     if (this.form.valid) {
       console.log('SUBMITTED FORM IS VALID');
     }
+  }
+
+  get isNameValid(): boolean {
+    return this.name?.touched && this.name?.valid;
+  }
+
+  get isNameInvalid(): boolean {
+    return this.name?.touched && !this.name?.valid;
+  }
+
+  get isEmailValid(): boolean {
+    return this.email?.touched && this.email?.valid;
+  }
+
+  get isEmailInvalid(): boolean {
+    return this.email?.touched && !this.email?.valid;
+  }
+
+  get isPasswordValid(): boolean {
+    return this.password?.touched && this.password?.valid;
+  }
+
+  get isPasswordInvalid(): boolean {
+    return this.password?.touched && !this.password?.valid;
+  }
+
+  get name(): AbstractControl {
+    return this.form?.controls?.name;
+  }
+
+  get password(): AbstractControl {
+    return this.form?.controls?.password;
+  }
+
+  get email(): AbstractControl {
+    return this.form?.controls?.email;
   }
 
 }
