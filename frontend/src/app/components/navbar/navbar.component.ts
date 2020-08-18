15a8fc530fc9d453530a46324fd5d0b8598dc863
Josiah Sayers <josiah.sayers15@gmail.com>
2020-08-17 23:36:12 -0400

    adds animation to the navbar hamburger menu

--- frontend/src/app/components/navbar/navbar.component.ts
+++ frontend/src/app/components/navbar/navbar.component.ts
@@ -1,25 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { AuthService } from '../../shared/services/auth/auth.service';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
+  animations: [
+    trigger('openClose', [
+      state('open', style({
+        maxHeight: '500px',
+        opacity: '100%'
+      })),
+      state('closed', style({
+        maxHeight: '0px',
+        opacity: '0%'
+      })),
+      transition('open => closed', [
+        animate('0.2s ease-in')
+      ]),
+      transition('closed => open', [
+        animate('0.2s ease-out')
+      ]),
+    ]),
+  ],
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent {
+
+  hamburgerState: 'open' | 'closed';
 
   constructor(
     private auth: AuthService
-  ) { }
-
-  ngOnInit(): void {
+  ) {
+    this.hamburgerState = 'closed';
   }
 
   hamburgerClick(event): void {
-    const hamburgerElement = event.target;
-    const menuElement = document.querySelector(`#${event.target.dataset.target}`);
+    const hamburgerElement = <HTMLElement>event.target;
+    const menuElement = <HTMLElement>document.querySelector(`#${event.target.dataset.target}`);
     hamburgerElement.classList.toggle('is-active');
     menuElement.classList.toggle('is-active');
+
+    this.hamburgerState = hamburgerElement.classList.contains('is-active') ? 'open' : 'closed';
   }
 
   get isLoggedIn(): boolean {
