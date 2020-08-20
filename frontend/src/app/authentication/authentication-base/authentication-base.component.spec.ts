bfa272a24e3255890cc90394d1d71752170f6ddb
Josiah Sayers <josiah.sayers15@gmail.com>
2020-08-20 16:29:05 -0400

    adds tests for pass fail list item

--- frontend/src/app/authentication/authentication-base/authentication-base.component.spec.ts
+++ frontend/src/app/authentication/authentication-base/authentication-base.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AuthenticationBaseComponent } from '@src/app/authentication/authentication-base/authentication-base.component';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('AuthenticationBaseComponent', () => {
   let component: AuthenticationBaseComponent;
@@ -8,9 +9,10 @@ describe('AuthenticationBaseComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AuthenticationBaseComponent ]
+      declarations: [AuthenticationBaseComponent],
+      imports: [RouterTestingModule]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
--- frontend/src/app/components/pass-fail-list-item/pass-fail-list-item.component.spec.ts
+++ frontend/src/app/components/pass-fail-list-item/pass-fail-list-item.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { PassFailListItemComponent } from '@src/app/components/pass-fail-list-item/pass-fail-list-item.component';
+import { MockComponent } from 'ng-mocks';
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
+import { faCheckSquare, faBan } from '@fortawesome/free-solid-svg-icons';
 
 describe('PassFailListItemComponent', () => {
   let component: PassFailListItemComponent;
@@ -8,9 +10,12 @@ describe('PassFailListItemComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PassFailListItemComponent ]
+      declarations: [
+        PassFailListItemComponent,
+        MockComponent(FaIconComponent)
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -21,5 +26,29 @@ describe('PassFailListItemComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('currentIcon', () => {
+    it('returns faCheckSquare when passing is true', () => {
+      component.passing = true;
+      expect(component.currentIcon).toBe(faCheckSquare);
+    });
+
+    it('returns faBan when passing is false', () => {
+      component.passing = false;
+      expect(component.currentIcon).toBe(faBan);
+    });
+  });
+
+  describe('currentTextClass', () => {
+    it('returns "has-text-success" when passing is true', () => {
+      component.passing = true;
+      expect(component.currentTextClass).toBe('has-text-success');
+    });
+
+    it('returns "has-text-danger" when passing is false', () => {
+      component.passing = false;
+      expect(component.currentTextClass).toBe('has-text-danger');
+    });
   });
 });
