556adf7af20256d03389297a65822910bf9018a2
Josiah Sayers <josiah.sayers15@gmail.com>
2020-10-14 15:13:20 -0400

    simplifies great things component structure

--- frontend/src/app/great-things/great-things.common.ts
+++ frontend/src/app/great-things/great-things.common.ts
@@ -2,11 +2,9 @@ import { Routes } from '@angular/router';
 import { GreatThingsBaseComponent } from '@src/app/great-things/great-things-base/great-things-base.component';
 import { GreatThingComponent } from '@src/app/great-things/great-thing/great-thing.component';
 import { GreatThingsGuard } from '@src/app/great-things/great-things-base/great-things.guard';
-import { GreatThingsTilesComponent } from '@src/app/great-things/great-things-tiles/great-things-tiles.component';
 
 export const componentDeclarations: any[] = [
   GreatThingsBaseComponent,
-  GreatThingsTilesComponent,
   GreatThingComponent
 ];
 
@@ -19,6 +17,5 @@ export const routes: Routes = [
 
 export const exports: any[] = [
   GreatThingsBaseComponent,
-  GreatThingComponent,
-  GreatThingsTilesComponent
+  GreatThingComponent
 ];
