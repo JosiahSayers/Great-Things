c2e0be9b07a0348f5a3c3d8eda42916e4a777fc5
Josiah Sayers <josiah.sayers15@gmail.com>
2020-10-15 19:12:33 -0400

    starts overview component

--- frontend/src/app/account/overview/overview.component.ts
+++ frontend/src/app/account/overview/overview.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '@src/app/shared/services/auth/auth.service';
 
 @Component({
   selector: 'app-overview',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OverviewComponent implements OnInit {
 
-  constructor() { }
+  photoHref: string;
+  name: string;
+  email: string;
+
+  constructor(
+    private auth: AuthService
+  ) { }
 
   ngOnInit(): void {
+    this.photoHref = this.auth.photoHref();
+    this.name = this.auth.fullName();
+    this.email = this.auth.emailAddress();
   }
 
 }
