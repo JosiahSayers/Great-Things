dd5e9d639d8e7b666fde974c816eb967d826a498
Josiah Sayers <josiah.sayers15@gmail.com>
2020-10-15 17:06:22 -0400

    updates spec imports

--- frontend/src/app/account/main/main.component.spec.ts
+++ frontend/src/app/account/main/main.component.spec.ts
@@ -1,4 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { MainComponent } from '@src/app/account/main/main.component';
 
@@ -8,9 +9,10 @@ describe('MainComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MainComponent ]
+      declarations: [MainComponent],
+      imports: [RouterTestingModule]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
--- frontend/src/app/shared/components/navbar/navbar-user-info/navbar-user-info.component.spec.ts
+++ frontend/src/app/shared/components/navbar/navbar-user-info/navbar-user-info.component.spec.ts
@@ -1,16 +1,26 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NavbarUserInfoComponent } from '@src/app/shared/components/navbar/navbar-user-info/navbar-user-info.component';
+import { spyOnClass } from '../../../../utils/testing/helper-functions';
+import { Spied } from '../../../../utils/testing/spied.interface';
+import { AuthService } from '../../../services/auth/auth.service';
 
 describe('NavbarUserInfoComponent', () => {
   let component: NavbarUserInfoComponent;
   let fixture: ComponentFixture<NavbarUserInfoComponent>;
+  let auth: Spied<AuthService>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ NavbarUserInfoComponent ]
+      declarations: [NavbarUserInfoComponent],
+      providers: [
+        {
+          provide: AuthService,
+          useFactory: () => auth = spyOnClass(AuthService)
+        }
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
