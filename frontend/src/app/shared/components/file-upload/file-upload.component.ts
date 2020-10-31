37e8f4a81e480cfc5fd85107bbd9e29b5cd5923b
Josiah Sayers <josiah.sayers15@gmail.com>
2020-10-30 22:34:40 -0400

    uses bulma file upload component and font awesome 5

--- frontend/src/app/shared/components/file-upload/file-upload.component.ts
+++ frontend/src/app/shared/components/file-upload/file-upload.component.ts
@@ -9,12 +9,15 @@ export class FileUploadComponent {
 
   @Output() fileSelected = new EventEmitter<File>();
   @Input() acceptFileType = '*';
+  fileName = 'Select a file...';
 
   onFileSelect(event): void {
     if (event.target.files.length > 0) {
-      const file = event.target.files[0];
+      const file = <File>event.target.files[0];
+      this.fileName = file.name;
       this.fileSelected.emit(file);
     } else {
+      this.fileName = 'Select a file...';
       this.fileSelected.emit(null);
     }
   }
